097d7921bdd1347a1ddd1afb5c97bbb08af610e3
Mike <mike@thirty8.co.uk>
2024-01-11 15:33:25 +0000

    Update another-test-plugin.php

--- another-test-plugin.php
+++ another-test-plugin.php
@@ -5,8 +5,9 @@
  * Description: Doesn't do anything, purely for testing git updater functionality
  * Version: 0.1
  * Author: Mike Ellis / The Museum Platform	 
+ * GitHub Plugin URI: https://github.com/dmje/another-test-plugin
  */
 
-GitHub Plugin URI: https://github.com/dmje/another-test-plugin
+
 	
 ?>
