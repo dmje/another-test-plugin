abb7c60e4ac378e20c1c1cdcd6015a88d91d6f8e
Mike <mike@thirty8.co.uk>
2024-01-11 15:26:17 +0000

    Create another-test-plugin.php

---
+++ another-test-plugin.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Plugin Name: Test plugin
+ * Plugin URI: https://themuseumplatform.com
+ * Description: Doesn't do anything, purely for testing git updater functionality
+ * Version: 0.1
+ * Author: Mike Ellis / The Museum Platform	 
+ */
+
+GitHub Plugin URI: https://github.com/dmje/another-test-plugin
+	
+?>
